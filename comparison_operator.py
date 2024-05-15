bf435fc2a05a8bd72455ad3888323434b309ce38
Amit Jha <amit.jhaa17@gmail.com>
2024-05-15 19:00:05 +0530

    if statments

---
+++ comparison_operator.py
@@ -0,0 +1,19 @@
+# > greater then
+# < less then
+# == equal to
+
+number1 = input('Enter number 1: ')
+number2 = input('Enter number 2: ')
+
+print("Number 1 is greater then equal to Number 2")
+print(number1 >= number2)
+
+print("Number 1 is less then equal to Number 2")
+print(number1 <= number2)
+
+
+print("Number 1 is equal to Number 2")
+print(number1 == number2)
+
+print("Number 1 is not equal to Number 2")
+print(number1 != number2)
---
+++ if_statment.py
@@ -0,0 +1,9 @@
+
+
+temperature = 10
+
+
+if(temperature >= 10):
+    print("Temprature is greater then: " +str(temperature))
+else:
+    print("Temprature is less then: " +str(temperature))
